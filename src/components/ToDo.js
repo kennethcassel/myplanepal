53a4a66c888d019af4cb5aed7650bd0a2a926741
Kenneth Cassel <kennethcassel@gmail.com>
2018-10-21 00:51:14 -0500

    github pages

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -13,6 +13,8 @@ import Checkbox from '@material-ui/core/Checkbox';
 // import IconButton from '@material-ui/core/IconButton';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+
 
 
 // CSS
@@ -90,6 +92,11 @@ const styles = theme => ({
             <Typography variant="h6" component="h5">
               To Do Before Flight
             </Typography>
+
+            <MessengerCustomerChat
+      pageId="2025835257495217"
+      appId="290859664860684"
+    />
             
             <List>
               {this.state.toDoArr.map(value => (
